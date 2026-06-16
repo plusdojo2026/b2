6599310922c1607a202e02c340cfc9326e8e08b6
plusdojo2026-watanabe <watanabe-sara-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 13:00:50 +0900

    BonusServletのdoGetまわり作成

--- source/src/main/java/servlet/BonusServlet.java
+++ source/src/main/java/servlet/BonusServlet.java
@@ -1,5 +1,30 @@
 package servlet;
 
-public class BonusServlet {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BonusServlet
+ */
+
+@WebServlet("/BonusServlet")
+public class BonusServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// ログインボーナスページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/bonus.jsp");
+		dispatcher.forward(request, response);
+	}
 }

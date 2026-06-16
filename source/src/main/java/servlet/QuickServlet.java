19e9e545441fb5e803801821b4c7e440d25ca648
plusdojo2026-watanabe <watanabe-sara-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 11:51:07 +0900

    QuickServletのdoGetメソッドまわりを追加
    QuickServletのdoGetメソッドを追加
    quicl.jspへフォワードするようにした
    quick.jspに仮入力

--- source/src/main/java/servlet/QuickServlet.java
+++ source/src/main/java/servlet/QuickServlet.java
@@ -1,5 +1,30 @@
 package servlet;
 
-public class QuickServlet {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+
+@WebServlet("/QuickServlet")
+public class QuickServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// ログインページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/quick.jsp");
+		dispatcher.forward(request, response);
+	}
 }
--- source/src/main/webapp/WEB-INF/jsp/quick.jsp
+++ source/src/main/webapp/WEB-INF/jsp/quick.jsp
@@ -4,9 +4,9 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>簡易記録入力</title>
 </head>
 <body>
-
+簡易記録入力画面
 </body>
 </html>

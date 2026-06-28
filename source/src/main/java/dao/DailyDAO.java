59a43c7e0f9ca8117ebc90a075df2ce37b20ad17
plusdojo2026-tanaka <tanaka-haruto-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-28 22:55:18 +0900

    0628_とりあえず版公開用

--- source/src/main/java/dao/DailyDAO.java
+++ source/src/main/java/dao/DailyDAO.java
@@ -153,9 +153,14 @@ public class DailyDAO {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
 			// データベースに接続する
+			
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/b2?"
 					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-					"root", "password");
+					"b2", "7grzQ32C9PWe9pdD");
+			
+//			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/b2?"
+//					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+//					"root", "password");
 
 			/*
 			 * ~SQL文~
